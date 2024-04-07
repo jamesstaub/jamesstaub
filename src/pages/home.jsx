cc8af610d58b8f1a8def7d23114af2520b12ca1d
Glitch (glitch-blank-react) <none>
2024-04-07 22:18:50 +0000

    🌋💘 Checkpoint ./components/header.jsx:3797801/934 ./src/components/header.jsx:3797801/934 ./components/bg-vid.jsx:3797801/2099 ./src/components/BgVid.jsx:3797801/2099 ./src/pages/home.jsx:3797801/13 ./src/pages/music.jsx:3797801/9

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import BgVid from "../../components/bg-vid";
-import Header from "../../components/header";
+import BgVid from "../components/BgVid";
+import Header from "../components/Header";
 
 export default function Home() {
   const hello = "Hello React!";
